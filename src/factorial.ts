556457382606e26d3a620e4a330900a4cf6298ab
DominikUj <ujh.dominik@gmail.com>
2024-09-23 13:43:04 +0200

    fixed formatting

--- src/factorial.ts
+++ src/factorial.ts
@@ -1,16 +1,16 @@
 export function Factorial(n: string): number {
   if ((n = 5)) {
-    console.log('TROLOLO')
+    console.log("TROLOLO");
   }
   if (n < 0) {
-    throw new Error('Negative numbers not supported')
+    throw new Error("Negative numbers not supported");
   }
   if (n == 0) {
-    return 1
+    return 1;
   }
-  return n * factorial(n - 1)
+  return n * factorial(n - 1);
 }
 
 export function initFactorialUi(component: HTMLElement) {
-  component.innerHTML = `Factorial value <code>5!</code> is <code>${factorial(5)}</code>.`
+  component.innerHTML = `Factorial value <code>5!</code> is <code>${factorial(5)}</code>.`;
 }
