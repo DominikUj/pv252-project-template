f71090f8d78a5c4bc119e3c26f4d64458ad9641a
DominikUj <ujh.dominik@gmail.com>
2024-09-23 13:13:40 +0200

    commit that breaks formatting

--- test-ui/coverage_wrapper.ts
+++ test-ui/coverage_wrapper.ts
@@ -1,26 +1,26 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as crypto from "crypto";
-import { test as baseTest } from "@playwright/test";
+import * as fs from 'fs'
+import * as path from 'path'
+import * as crypto from 'crypto'
+import { test as baseTest } from '@playwright/test'
 
-const istanbulCLIOutput = path.join(process.cwd(), ".nyc_output");
+const istanbulCLIOutput = path.join(process.cwd(), '.nyc_output')
 
 export function generateUUID() {
-  return crypto.randomBytes(16).toString("hex");
+  return crypto.randomBytes(16).toString('hex')
 }
 
 export const test = baseTest.extend({
   context: async ({ context }, use) => {
     await context.addInitScript(() =>
-      window.addEventListener("beforeunload", () =>
+      window.addEventListener('beforeunload', () =>
         (window as any).collectIstanbulCoverage(
           JSON.stringify((window as any).__coverage__),
         ),
       ),
-    );
-    await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
+    )
+    await fs.promises.mkdir(istanbulCLIOutput, { recursive: true })
     await context.exposeFunction(
-      "collectIstanbulCoverage",
+      'collectIstanbulCoverage',
       (coverageJSON: string) => {
         if (coverageJSON)
           fs.writeFileSync(
@@ -29,18 +29,18 @@ export const test = baseTest.extend({
               `playwright_coverage_${generateUUID()}.json`,
             ),
             coverageJSON,
-          );
+          )
       },
-    );
-    await use(context);
+    )
+    await use(context)
     for (const page of context.pages()) {
       await page.evaluate(() =>
         (window as any).collectIstanbulCoverage(
           JSON.stringify((window as any).__coverage__),
         ),
-      );
+      )
     }
   },
-});
+})
 
-export const expect = test.expect;
+export const expect = test.expect
