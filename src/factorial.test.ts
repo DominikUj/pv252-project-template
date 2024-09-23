26a2ecf65603207533fe7de554a93a9729755011
DominikUj <ujh.dominik@gmail.com>
2024-09-23 13:41:19 +0200

    kill unit test

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -1,12 +1,12 @@
-import { factorial } from "./factorial.ts";
+import { factorial } from './factorial.ts'
 
-test("factorial-5", () => {
-  expect(factorial(5)).toBe(120);
-});
+test('factorial-5', () => {
+  expect(factorial(5)).toBe(121)
+})
 
-test("factorial-minus", () => {
+test('factorial-minus', () => {
   const will_throw = () => {
-    factorial(-1);
-  };
-  expect(will_throw).toThrow("Negative numbers not supported");
-});
+    factorial(-1)
+  }
+  expect(will_throw).toThrow('Negative numbers not supported')
+})
